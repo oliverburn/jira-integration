4ca446bbf5d234e37028a23e79fc61263ca27920
Oliver Burn <oburn@atlassian.com>
2018-04-26 09:58:52 +1000

    Working tests

---
+++ src/test/java/blah/RunnerTest.java
@@ -0,0 +1,13 @@
+package blah;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunnerTest {
+
+    @Test
+    public void main() {
+        assertTrue(true);
+    }
+}
